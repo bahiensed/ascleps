fff14276f3a9a0fc003509d569180613923d4eda
dbahiense <douglas@rohling.com.br>
2024-10-15 08:56:58 -0300

    style: change header and footer layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Leaf } from "lucide-react";
 
 const Header = () => {
   return (
-    <div className="absolute bg-gray-100 flex items-center p-2 shadow-md justify-between w-full">
+    <div className="bg-gray-100 fixed flex items-center p-2 shadow-md justify-between w-full">
       <Link href={"/"} className="flex gap-1 items-center">
         <Leaf />
         <h3 className="font-bold text-xl tracking-tight">Ascleps</h3>
