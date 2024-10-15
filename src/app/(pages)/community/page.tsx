9975321af391d4cea61839bcaa014e8f31905132
dbahiense <douglas@rohling.com.br>
2024-10-15 13:51:44 -0300

    style: play with layout

--- src/app/(pages)/community/page.tsx
+++ src/app/(pages)/community/page.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image"
+import Link from "next/link"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
-import Link from "next/link"
+import coverImage from "../../../../public/images/users/1719348954222/1719348954222.jpeg"
 
 const Community = () => {
   return (
@@ -10,29 +12,37 @@ const Community = () => {
         Community
       </h1>
 
-      <section className="grid grid-cols-2 gap-4">
-        <Card>
-          <div  className="flex justify-center mt-4">
-          <Avatar className="w-16 h-16">
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+      <section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card className="relative">
+          <div className="relative h-12 w-full rounded-t-lg overflow-hidden">
+            <Image
+              src={coverImage}
+              alt="Cover Image"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+          </div>
+
+          <div className="relative flex justify-center mt-[-2rem]">            
+            <Avatar className="w-14 h-14">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
           </div>
         </Card>
 
         <Card>
             <div  className="flex justify-center mt-4">
               <Avatar className="w-16 h-16">
-                <AvatarImage src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb" />
+                <AvatarImage src="https://github.com/shadcn.png" />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
             </div>
 
-          <CardContent className="border border-red-500">
+          <CardContent>
             <p>Card Content</p>
           </CardContent>
 
-          <CardFooter className="border border-red-500">
+          <CardFooter>
             <p>Card Footer</p>
           </CardFooter>
         </Card>
@@ -40,8 +50,8 @@ const Community = () => {
         <Card>
           <div  className="flex justify-center mt-4">
           <Avatar className="w-16 h-16">
-            <AvatarImage src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarImage src="https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvb2F1dGhfZ29vZ2xlL2ltZ18ybUxQMFdwbEZuSkJCdG41VEVqa0VZTThRSWgifQ?width=80" />
+            <AvatarFallback>DB</AvatarFallback>
           </Avatar>
           </div>
         </Card>
