f9ed1cab04b2e539dd69d0c5843a15a0b527a783
dbahiense <douglas@rohling.com.br>
2024-10-16 16:25:41 -0300

    chore: add connections and recomendations data

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-import { BookHeart, BookText, Calculator, CirclePlus, CircleUserRound, Gamepad2, Pill, ShoppingBag, SquareActivity, TableProperties, Trophy, UsersRound } from "lucide-react";
+import { BookHeart, BookText, Calculator, CircleUserRound, Gamepad2, Pill, ShoppingBag, SquareActivity, TableProperties, Trophy, UsersRound } from "lucide-react";
 
 export const words: string[] = [
   "Aprender", "Se divertir", "Passar em Concursos",
@@ -9,10 +9,117 @@ export const words: string[] = [
 
 
 export const connections = [
-  { name: "", }  
+  {
+    id: "1719348954222",
+    cover: "/images/users/1719348954222/cover.jpeg",
+    avatar: "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvb2F1dGhfZ29vZ2xlL2ltZ18ybUxQMFdwbEZuSkJCdG41VEVqa0VZTThRSWgifQ?width=80",
+    fallback: "DB",
+    name: "Douglas Bahiense",
+    position: "R3 no INCA",
+    almaMater: "Faculdade de Medicina de Petrópolis"
+  },
+  {
+    id: "1719348954223",
+    cover: "/images/users/1719348954223/cover.png",
+    avatar: "https://github.com/shadcn.png",
+    fallback: "TA",
+    name: "Teodoro Adorno",
+    position: "Professor no Instituto para Pesquisas Sociais",
+    almaMater: "Universidade de Frankfurt"
+  },
+  {
+    id: "1719348954225",
+    cover: "/images/users/1719348954225/cover.png",
+    avatar: "/images/users/1719348954225/profile.png",
+    fallback: "CB",
+    name: "Carlos Bruno Nogueira",
+    position: "Neurologista",
+    almaMater: "Universidade Federal Fluminense"
+  },
+  {
+    id: "1719348954224",
+    cover: "/images/users/1719348954224/cover.png",
+    avatar: "/images/users/1719348954224/profile.png",
+    fallback: "AB",
+    name: "Amanda Bicudo",
+    position: "Endocrinologista",
+    almaMater: ""
+  },
+  {
+    id: "1719348954223",
+    cover: "/images/users/1719348954223/cover.png",
+    avatar: "https://github.com/shadcn.png",
+    fallback: "TA",
+    name: "Teodoro Adorno",
+    position: "",
+    almaMater: ""
+  },
+  {
+    cover: "/images/users/1719348954223/cover.png",
+    avatar: "https://github.com/shadcn.png",
+    fallback: "TA",
+    name: "Teodoro Adorno",
+    position: "",
+    almaMater: ""
+  }
 ]
 
-export const recomendations = []
+export const recomendations = [
+  {
+    id: "1719348954223",
+    cover: "/images/users/1719348954223/cover.png",
+    avatar: "https://github.com/shadcn.png",
+    fallback: "TA",
+    name: "Teodoro Adorno",
+    position: "Professor no Instituto para Pesquisas Sociais",
+    almaMater: "Universidade de Frankfurt"
+  },
+  {
+    id: "1719348954223",
+    cover: "/images/users/1719348954223/cover.png",
+    avatar: "https://github.com/shadcn.png",
+    fallback: "TA",
+    name: "Teodoro Adorno",
+    position: "Professor no Instituto para Pesquisas Sociais",
+    almaMater: "Universidade de Frankfurt"
+  },
+  {
+    id: "1719348954223",
+    cover: "/images/users/1719348954223/cover.png",
+    avatar: "https://github.com/shadcn.png",
+    fallback: "TA",
+    name: "Teodoro Adorno",
+    position: "Professor no Instituto para Pesquisas Sociais",
+    almaMater: "Universidade de Frankfurt"
+  },
+  {
+    id: "1719348954223",
+    cover: "/images/users/1719348954223/cover.png",
+    avatar: "https://github.com/shadcn.png",
+    fallback: "TA",
+    name: "Teodoro Adorno",
+    position: "Professor no Instituto para Pesquisas Sociais",
+    almaMater: "Universidade de Frankfurt"
+  },
+  {
+    id: "1719348954223",
+    cover: "/images/users/1719348954223/cover.png",
+    avatar: "https://github.com/shadcn.png",
+    fallback: "TA",
+    name: "Teodoro Adorno",
+    position: "Professor no Instituto para Pesquisas Sociais",
+    almaMater: "Universidade de Frankfurt"
+  },
+  {
+    id: "1719348954223",
+    cover: "/images/users/1719348954223/cover.png",
+    avatar: "https://github.com/shadcn.png",
+    fallback: "TA",
+    name: "Teodoro Adorno",
+    position: "Professor no Instituto para Pesquisas Sociais",
+    almaMater: "Universidade de Frankfurt"
+  },
+]
 
 
 
@@ -24,18 +131,19 @@ export const guildes = []
 
 export const footerLinks = [
   { href: "profile", icon: CircleUserRound, name: "Profile" },
-  { href: "community", icon: UsersRound, name: "Community" },
+  { href: "network", icon: UsersRound, name: "Network" },
   { href: "round", icon: SquareActivity, name: "Round" },
   { href: "challenges", icon: Trophy, name: "Challenges" },
   { href: "marketplace", icon: ShoppingBag, name: "Marketplace" },
 ]
 
 export const moreLinks = [
-  { href: "profile", icon: CircleUserRound, name: "Profile" },
-  { href: "community", icon: UsersRound, name: "Community" },
-  { href: "round", icon: SquareActivity, name: "Round" },
-  { href: "challenges", icon: Trophy, name: "Challenges" },
-  { href: "marketplace", icon: ShoppingBag, name: "Marketplace" },
+  { href: "games", icon: Gamepad2, name: "Games" },
+  { href: "rankings", icon: TableProperties, name: "Rankings" },
+  { href: "calculators", icon: Calculator, name: "Calculators" },
+  { href: "icd", icon: BookText, name: "ICD-11" },
+  { href: "pharmacopoeias", icon: Pill, name: "Pharmacopoeias" },
+  { href: "other-lists", icon: BookHeart, name: "Other Lists" },
 ]
 
 
